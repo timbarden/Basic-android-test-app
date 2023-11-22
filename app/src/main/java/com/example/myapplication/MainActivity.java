02db0b6b82440882705a594758bf7c1d0147381b
Tim Barden <tim.barden@junipereducation.org>
2023-11-22 15:54:41 +0000

    add google services and initialize firebase

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.FirebaseApp;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Add this in your onCreate() method or Application class
+        FirebaseApp.initializeApp(this);
+
         setContentView(R.layout.activity_main);
     }
 }
