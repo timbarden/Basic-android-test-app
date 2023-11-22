294d4fc60393e1f99ae2b6bc2424885f35ce1792
Tim Barden <tim.barden@junipereducation.org>
2023-11-22 16:21:49 +0000

    add FirebaseMessaging

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,16 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Add this in your onCreate() method or Application class
+
         FirebaseApp.initializeApp(this);
+
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.w("FCMToken", "Fetching FCM registration token failed", task.getException());
+                return;
+            }
+            // Get new FCM registration token
+            String token = task.getResult();
+            Log.d("FCMToken", token);
+            // Send this token to your server to send notifications
+        });
 
         setContentView(R.layout.activity_main);
     }
