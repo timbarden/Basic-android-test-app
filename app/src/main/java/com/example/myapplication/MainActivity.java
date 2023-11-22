862a66677f1f766b0821ab9af7e582e6e18ffb5c
Tim Barden <tim.barden@junipereducation.org>
2023-11-22 18:32:49 +0000

    add post notification permission dialog

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +33,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
         setContentView(R.layout.activity_main);
+
+        // Add permission request dialog for notifications, so it runs after user has added a school
+        if (!checkPostNotificationsPermission(getApplicationContext())) {
+            requestPostNotificationsPermission();
+        }
     }
+
+    public boolean checkPostNotificationsPermission(Context context) {
+        // Permissions need to be requested for Android 13+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+          return ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED;
+        } else {
+          return true;
+        }
+    }
+  
+    // POST_NOTIFICATIONS is classified as a 'dangerous' permission so much be manually requested from the user
+    public void requestPostNotificationsPermission() {
+      Log.d("requestPostNotificationsPermission", "");
+      requestPermissions( 
+        new String[] { 
+          android.Manifest.permission.POST_NOTIFICATIONS 
+        }, 
+        255);
+    }
+        
 }
