59616449332cd1ca19a85fa73c35300f96867ae0
Tim Barden <tim.barden@junipereducation.org>
2023-11-23 07:58:54 +0000

    update firebase messaging class

--- app/src/main/java/com/example/myapplication/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/myapplication/MyFirebaseMessagingService.java
@@ -1,13 +1,20 @@
+package com.example.myapplication;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import android.util.Log;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d("onMessageReceived", "");
-        // Handle received messages here
-        // You can access the message data using remoteMessage.getData()
+    }
+
+    @Override
+    public void onNewToken(String newToken) {
+        super.onNewToken(newToken);
+        Log.d("onNewToken", newToken);
     }
 }
